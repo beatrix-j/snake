850ea4aac7f1afc04a843e20ecbe08a3022479d7
Beatrix Jones <beatrix.jones@icloud.com>
2025-01-12 13:43:53 +1100

    Initial Commit

---
+++ snakegame/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  snakegame
+//
+//  Created by Beatrix Jones on 12/1/2025.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <ncurses.h>
+int main(int argc, const char * argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
